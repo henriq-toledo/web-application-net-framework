a08e94592cd63be4e19309a430bc6e0e9caf6453
Henrique Toledo <henriq_toledo@yahoo.com.br>
2021-11-18 08:41:30 -0300

    Add the .NET Framework MVC web application project with transform and unit test project.

---
+++ src/WebApplicationNetFramework/WebApplicationNetFramework.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebApplicationNetFramework.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApplicationNetFramework.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("75921846-dc00-4ef5-a6f0-3abb0d8b1d3d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/WebApplicationNetFramework/WebApplicationNetFramework/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace WebApplicationNetFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = ConfigurationManager.AppSettings["Environment"];
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
